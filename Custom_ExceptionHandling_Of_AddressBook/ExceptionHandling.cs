32506cb33291ade11d4cc6fb40f9f67e034a016a
“rohithgopala44” <rohithgopala44@gmail.com>
2023-06-16 18:29:49 +0530

    Added: Program Of Address Book By Handling The Generated Exceptions

---
+++ Custom_ExceptionHandling_Of_AddressBook/ExceptionHandling.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_ExceptionHandling_Of_AddressBook
+{
+    public class ExceptionHandling
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+
+    class AddressBook
+    {
+        private ExceptionHandling[] entries;
+        private int temp;
+
+
+        public AddressBook()
+        {
+            entries = new ExceptionHandling[05];
+            temp = 0;
+        }
+
+
+        public void AddDetails(ExceptionHandling entry)
+        {
+            if (temp < entries.Length)
+            {
+                entries[temp] = entry;
+                temp++;
+                Console.WriteLine("Entry added successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Address book is full. Not able to add more entries.");
+            }
+        }
+
+        public void EditDetails(string firstName)
+        {
+            try
+            {
+                if (EditDetails == null)
+                {
+                    throw new CustomException("Invalid Input");
+                }
+                bool entryFound = false;
+
+                for (int i = 0; i <= temp; i++)
+                {
+                    if (entries[i].FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Enter updated last name:");
+                        entries[i].LastName = Console.ReadLine();
+
+                        Console.WriteLine("Enter updated contact number:");
+                        entries[i].ContactNumber = Console.ReadLine();
+
+                        Console.WriteLine("Enter updated email name:");
+                        entries[i].Email = Console.ReadLine();
+
+                        Console.WriteLine("Enter updated city:");
+                        entries[i].City = Console.ReadLine();
+
+                        Console.WriteLine("Enter updated state:");
+                        entries[i].State = Console.ReadLine();
+
+                        Console.WriteLine("Entry updated successfully.");
+                        entryFound = true;
+                        break;
+                    }
+                }
+
+                if (!entryFound)
+                {
+                    Console.WriteLine("Entry not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Custom Exception : " + ex.Message);
+            }
+        }
+
+        public void ViewAllDetailsEntered()
+        {
+            if (temp == 0)
+            {
+                Console.WriteLine("Address book is empty.");
+                return;
+            }
+
+            Console.WriteLine("All Entries");
+
+            for (int i = 0; i < temp; i++)
+            {
+                Console.WriteLine($"First Name: {entries[i].FirstName}");
+                Console.WriteLine($"Last Name: {entries[i].LastName}");
+                Console.WriteLine($"Contact Number: {entries[i].ContactNumber}");
+                Console.WriteLine($"Email: {entries[i].Email}");
+                Console.WriteLine($"City: {entries[i].City}");
+                Console.WriteLine($"State: {entries[i].State}");
+                Console.WriteLine();
+            }
+        }
+
+        public void DeleteDetails(string firstName)
+        {
+            bool entryFound = false;
+
+            for (int i = 0; i < temp; i++)
+            {
+                if (entries[i].FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase))
+                {
+                    for (int j = i; j < temp - 1; j++)
+                    {
+                        entries[j] = entries[j + 1];
+                    }
+
+                    temp--;
+                    Console.WriteLine("Entry delated successfully.");
+                    entryFound = true;
+                    break;
+                }
+            }
+
+            if (!entryFound)
+            {
+                Console.WriteLine("Entry not found.");
+            }
+        }
+    }
+}
---
+++ Custom_ExceptionHandling_Of_AddressBook/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Custom_ExceptionHandling_Of_AddressBook
+{
+    public class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBookProblem = new AddressBook();
+
+
+            while (true)
+            {
+                Console.WriteLine("Address Book Menu");
+                Console.WriteLine("1. Add Details");
+                Console.WriteLine("2. Edit Details");
+                Console.WriteLine("3. View Entered Details");
+                Console.WriteLine("4. Delete Details");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Enter your choice (1-5):");
+
+                try
+                {
+
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice >= 6)
+                    {
+                        throw new CustomException("Invalid Input");
+                    }
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter first name");
+                            string firstName = Console.ReadLine();
+
+                            Console.WriteLine("Enter last name:");
+                            string lastName = Console.ReadLine();
+
+                            Console.WriteLine("Enter ContactNumber");
+                            string contactnumber = Console.ReadLine();
+
+                            Console.WriteLine("Enter Email");
+                            string email = Console.ReadLine();
+
+                            Console.WriteLine("Enter City");
+                            string city = Console.ReadLine();
+
+                            Console.WriteLine("Enter State");
+                            string state = Console.ReadLine();
+
+                            ExceptionHandling entry = new ExceptionHandling
+                            {
+                                FirstName = firstName,
+                                LastName = lastName,
+                                ContactNumber = contactnumber,
+                                Email = email,
+                                City = city,
+                                State = state,
+                            };
+                            addressBookProblem.AddDetails(entry);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter first name of the entry to edit:");
+                            string editFirstName = Console.ReadLine();
+                            addressBookProblem.EditDetails(editFirstName);
+                            break;
+
+                        case 3:
+                            addressBookProblem.ViewAllDetailsEntered();
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Enter first name of the entry to delete:");
+                            string deleteFirstName = Console.ReadLine();
+                            addressBookProblem.DeleteDetails(deleteFirstName);
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Exiting program.");
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+
+                catch (CustomException ex)
+                {
+                    Console.WriteLine("Invalid Input please enter between 1-5 ");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("General Exception caught:" + ex.Message);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
